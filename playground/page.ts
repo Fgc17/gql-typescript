be8ff46777be6943dc6b7696b8a188b07dc86016
fgc17 <fgcoelho1709@outlook.com>
2024-08-10 23:42:21 +0000

    chore: playground update

--- playground/page.ts
+++ playground/page.ts
@@ -2,3 +2,12 @@
 import { GQLSelect, gql } from "../src/client/client";
 
 const rest = null as any;
+
+const query = gql("mutation", {
+  createPhysicalEvaluation: {
+    data: {
+      height: true,
+      imc: true,
+    },
+  },
+});
